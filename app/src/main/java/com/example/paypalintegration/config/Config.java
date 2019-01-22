2677a8ff52bd9caed7dc7f282d93d8cf6649e629
sasmita22 <hiroshisasmita@gmail.com>
2019-01-22 07:23:49 +0700

    first, start trying

---
+++ app/src/main/java/com/example/paypalintegration/config/Config.java
@@ -0,0 +1,5 @@
+package com.example.paypalintegration.config;
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID = "ASHwvxGpa8y26BWK3OoKh5NyfmQYWyuNBFLCV7nBQHP29RiGz1F7w0vSPa4vAKDPwISwHQKeBnr6Hm__";
+}
